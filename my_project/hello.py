5d4ab3166a6d70642dbb42cde0cefaf0c9e66e02
soyoon choi <soyoon.choi@code.berlin>
2019-04-11 03:18:30 +0200

    added comments page

--- my_project/hello.py
+++ my_project/hello.py
@@ -4,3 +4,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/comments/')
+def comments():
+    return render_template('comments.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
