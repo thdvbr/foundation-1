ab1571df14cff7ba4e72258b55c2e18b78046e28
soyoon choi <soyoon.choi@code.berlin>
2019-04-18 15:12:46 +0200

    made RA API call and comments feature

--- my_project/hello.py
+++ my_project/hello.py
@@ -1,13 +1,33 @@
-from flask import Flask , render_template
+from flask import Flask , redirect, render_template, request, url_for
 app = Flask(__name__)
+app.config["DEBUG"] = True
 
-@app.route('/')
+comments_list = []
+
+@app.route('/', methods=["GET", "POST"])
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
+    
 
-@app.route('/comments/')
+
+
+@app.route('/comments/', methods=["GET", "POST"])
 def comments():
-    return render_template('comments.html')
+    if request.method == "GET":
+        return render_template('comments.html', comments_list=comments_list)
+    #if "POST" 
+    else:
+        comments_list.append(request.form["comment"])
+    #request this page again, using GET 
+    return redirect(url_for("comments"))
+
+@app.route('/tour-dates/', methods=["GET", "POST"])
+def tour_dates():
+    return render_template("tour-dates.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
